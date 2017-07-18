4db762a28bb2b7215c4e6b429a40496e7eb6d327
edimchenko <edimchenko@soloten.com>
2017-07-18 22:49:50 +0300

    fix

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -3,12 +3,29 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use nill\widgetapn\Widgetapn;
+use yii\bootstrap\Carousel;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Новостной Блог';
 $this->params['breadcrumbs'][] = $this->title;
+
+foreach(\common\models\NewsBlog::find()->all() as $one){
+    $items[] =  [
+        'content' => Html::img(\yii\helpers\Url::toRoute(['/img/background.jpg']),['width' => '100%']),
+        'caption' => '<h2>'.$one->title.'</h2><h3>'.$one->content.'</h3><h6>('.$one->publiched_at.')</h6>',
+        'options' => [
+            'style' => 'height:500px;',
+            'class' => 'carousel slide',
+            'data-interval' => '3000'
+        ],
+        'controls' => [
+            '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
+            '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
+        ]
+    ];
+}
 ?>
 <div class="news-blog-index">
 
@@ -18,19 +35,30 @@ $this->params['breadcrumbs'][] = $this->title;
         <? //Html::a('Create News Blog', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute' => "user_id",
-                'value' => function($model) { return $model->user->username; },
-                'header' => "Имя"
-            ],
-            'title',
-            'content',
-            'publiched_at',
-            //['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+    <div class="row">
+        <?php echo Carousel::widget([
+            'items' => $items,
+            'options' => [
+                'style' => 'width:100%; margin:0 auto;' // Задаем ширину контейнера
+            ]
+        ]);
+        ?>
+    </div>
+
+<!--    --><?//= GridView::widget([
+//        'dataProvider' => $dataProvider,
+//        'columns' => [
+//            ['class' => 'yii\grid\SerialColumn'],
+//            [
+//                'attribute' => "user_id",
+//                'value' => function($model) { return $model->user->username; },
+//                'header' => "Имя"
+//            ],
+//            'title',
+//            'content',
+//            'publiched_at',
+//            //['class' => 'yii\grid\ActionColumn'],
+//        ],
+//    ]);
+    ?>
 </div>
